c1cf453e33ff7487a8aac26f1e2ebddf75bcac27
Thomas Krijnen <t.krijnen@gmail.com>
2022-09-19 12:54:23 +0200

    Cleanup

--- test/__main__.py
+++ test/__main__.py
@@ -1,20 +1,4 @@
-import os
-import glob
-
 import pytest
 
-from ..main import run
-
-test_files = glob.glob(os.path.join(os.path.dirname(__file__), "files/*.ifc"))
-@pytest.mark.parametrize("filename", test_files)
-def test_invocation(filename):
-    results = list(run(filename))
-    base = os.path.basename(filename)
-    print(base, len(results))
-    if base.startswith("fail-"):
-        assert len(results) > 0
-    elif base.startswith("pass-"):
-        assert len(results) == 0
-
 if __name__ == "__main__":
-    pytest.main(["-s", "-x", __file__])
+    pytest.main(["-s", "-x", "test_main.py"])
---
+++ test/test_main.py
@@ -0,0 +1,20 @@
+import os
+import glob
+
+import pytest
+
+from ..main import run
+
+test_files = glob.glob(os.path.join(os.path.dirname(__file__), "files/*.ifc"))
+@pytest.mark.parametrize("filename", test_files)
+def test_invocation(filename):
+    results = list(run(filename))
+    base = os.path.basename(filename)
+    print(base, len(results))
+    if base.startswith("fail-"):
+        assert len(results) > 0
+    elif base.startswith("pass-"):
+        assert len(results) == 0
+
+if __name__ == "__main__":
+    pytest.main(["-s", "-x", __file__])

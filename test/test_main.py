dd54c7419128f47ed1689c514ed0f732dbc6847e
Thomas Krijnen <t.krijnen@gmail.com>
2022-11-09 11:30:26 +0100

    Pretty test formatting

--- test/test_main.py
+++ test/test_main.py
@@ -3,6 +3,7 @@ import glob
 import sys
 
 import pytest
+import tabulate
 
 try:
     from ..main import run
@@ -15,7 +16,16 @@ test_files = glob.glob(os.path.join(os.path.dirname(__file__), "files/*.ifc"))
 def test_invocation(filename):
     results = list(run(filename))
     base = os.path.basename(filename)
-    print(base, len(results))
+    
+    print()
+    print(f"{len(results)} errors")
+    if results:
+        print(tabulate.tabulate(
+            [[c or '' for c in r] for r in results],
+            maxcolwidths=[30] * len(results[0]),
+            tablefmt="simple_grid"
+        ))
+    
     if base.startswith("fail-"):
         assert len(results) > 0
     elif base.startswith("pass-"):

181d18afa3112a1fb674e7506f37de9504d4e8d1
Geert Hesselink <geert.hess@gmail.com>
2023-01-10 14:57:39 +0100

    add testfiles, readme and description

--- features/GRF001_identical-coordinate-operations.feature
+++ features/GRF001_identical-coordinate-operations.feature
@@ -1,6 +1,11 @@
 @GRF
 Feature: GRF - Identical coordinate operations for all representation contexts
 
+"""
+The rule verifies that the same coordinate system is used within an IFC model. 
+And not, for example, an IfcMapConversion in one representation context and IfcRigidOperation in another.
+"""
+
   Scenario: IfcGeometricRepresentationContext
 
       Given A file with Schema Identifier "IFC4X3" or "IFC4X3_TC1" or "IFC4X3_ADD1" or "IFC4x1"
--- test/files/grf001/generate.py
+++ test/files/grf001/generate.py
@@ -4,8 +4,8 @@ import ifcopenshell.template
 import itertools
 
 
-def create_ifc(filename, mapconversion=0):
-    file = ifcopenshell.template.create(schema_identifier="IFC4X1")
+def create_ifc(filename, mapconversion=0, rigidoperation = 0):
+    file = ifcopenshell.template.create(schema_identifier="IFC4X3_ADD1")
 
     context = file.by_type("IfcGeometricRepresentationContext")[0]
     owner = file.by_type("IfcOwnerHistory")[0]
@@ -23,20 +23,39 @@ def create_ifc(filename, mapconversion=0):
         TrueNorth = TrueNorth 
     )
                                             
-    if mapconversion > 0:
+    if (mapconversion + rigidoperation) > 0:
         file.createIfcProjectedCrs(
-            Name = 'EPSG:31467',
+            Name = 'EPSG:3857', # plz 04177
+            GeodeticDatum = 'WGS84',
+            MapProjection = 'WSG',
             MapZone = '3', # GausKrueger
             MapUnit = file.by_type("IFCSIUNIT")[0]
         )
-            
+
+    n = 0       
     for i in range(mapconversion):
         file.createIfcMapConversion(
-            file.by_type('IfcGeometricRepresentationContext')[i],
-            file.by_type('IfcProjectedCrs')[0]
+            SourceCRS = file.by_type('IfcGeometricRepresentationContext')[i],
+            TargetCRS = file.by_type('IfcProjectedCrs')[0],
+            Northings = 5690966.11,
+            Eastings = 316131.64,
+            XAxisAbscissa = 1,
+            XAxisOrdinate = 0
         )
+        n += 1
+
+    for i in range(rigidoperation):
+        file.createIfcRigidOperation(
+            SourceCRS = file.by_type('IfcGeometricRepresentationContext')[i + n],
+            TargetCRS = file.by_type('IfcProjectedCrs')[0],
+        )
+
     file.write(filename)
 
-create_ifc('pass-grf001-2-None.ifc')
+create_ifc('pass-grf001-2-none.ifc')
 
-create_ifc('pass-grf001-2-Mapconversion.ifc', mapconversion=2)
+create_ifc('pass-grf001-2-mapconversion.ifc', mapconversion=2)
+
+create_ifc('fail-grf001-1-mapconversion-1-rigidoperation.ifc', mapconversion = 1, rigidoperation = 1)
+
+create_ifc('fail-grf001-1-rigidoperation-1-none.ifc', rigidoperation=1)

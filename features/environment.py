a488fe395d3c6b18211851074e275734b54f7a8d
Thomas Krijnen <t.krijnen@gmail.com>
2023-01-27 11:12:30 +0100

    Clear stack frames in between features

--- features/environment.py
+++ features/environment.py
@@ -3,6 +3,11 @@ import ifcopenshell
 from behave.model import Scenario
 
 def before_feature(context, feature):
+    # @tfk we have this strange issue between stack frames blending over
+    # between features so we need to preserve only the bottom two stack
+    # frames when beginning a new feature.
+    context._stack = context._stack[-2:]
+    
     context.model = ifcopenshell.open(context.config.userdata["input"])
     Scenario.continue_after_failed_step = True
 

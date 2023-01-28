a4e621d953ce16ce9126de47b6c34af4730a6edc
Geert Hesselink <geert.hess@gmail.com>
2023-01-28 21:58:29 +0100

    identical_unique to constraint for better reuse

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -285,7 +285,6 @@ def step_impl(context, entity_opt_stmt, insts = False):
     except:
         context.instances = []
 
-    #@todo check if setting this in global gives desired output
     context.within_model = getattr(context, 'within_model', True) and within_model
 
 @given('Its attribute {attribute}')
@@ -512,39 +511,47 @@ def unpack_tuple(tup):
             return item
 
 
-@then("The values must be {identical_or_unique}")
-def step_impl(context, identical_or_unique):
+@then("The value must {constraint}")
+@then("The values must {constraint}")
+@then('At least "{num:d}" value must {constraint}')
+@then('At least "{num:d}" values must {constraint}')
+def step_impl(context, constraint, num=None):
     errors = []
+
+    within_model = getattr(context, 'within_model', False)
+
+    if constraint.startswith('be '):
+        constraint = constraint[3:]
 
     if getattr(context, 'applicable', True):
         stack_tree = list(
             filter(None, list(map(lambda layer: layer.get('instances'), context._stack))))
-        instances = [context.instances] if getattr(
-            context, 'within_model', False) else context.instances
-        for i, values in enumerate(instances):
-            if not values:
-                continue
-            attribute = getattr(context, 'attribute', None)
-            if (identical_or_unique == 'identical' and not all([values[0] == i for i in values])):
-                incorrect_values = values # a more general approach of going through stack frames to return relevant information in error message?
-                incorrect_insts = stack_tree[-1]
-                errors.append(identical_values_error(incorrect_insts, incorrect_values, attribute,))
-            if identical_or_unique == 'unique':
-                seen = set()
-                duplicates = [x for x in values if x in seen or seen.add(x)]
-                if not duplicates:
+        instances = [context.instances] if within_model else context.instances
+
+        if constraint in ('identical', 'unique'):
+            for i, values in enumerate(instances):
+                if not values:
                     continue
-                inst_tree = [t[i] for t in stack_tree]
-                inst = inst_tree[-1]
-                false_instances = [inst_tree[1][i]
-                                   for i, x in enumerate(values) if x in duplicates]
-                incorrect_values = duplicates
-                incorrect_insts = false_instances  # instance where the duplicate value is found
-                # avoid mentioning ifcopenshell.entity_instance twice in error message
-                report_incorrect_insts = any(map_state(values, lambda v: do_try(
-                    lambda: isinstance(v, ifcopenshell.entity_instance), False)))
-                errors.append(duplicate_value_error(inst, incorrect_values, attribute,
-                             incorrect_insts, report_incorrect_insts))
+                attribute = getattr(context, 'attribute', None)
+                if (constraint == 'identical' and not all([values[0] == i for i in values])):
+                    incorrect_values = values # a more general approach of going through stack frames to return relevant information in error message?
+                    incorrect_insts = stack_tree[-1]
+                    errors.append(identical_values_error(incorrect_insts, incorrect_values, attribute,))
+                if constraint == 'unique':
+                    seen = set()
+                    duplicates = [x for x in values if x in seen or seen.add(x)]
+                    if not duplicates:
+                        continue
+                    inst_tree = [t[i] for t in stack_tree]
+                    inst = inst_tree[-1]
+                    incorrect_insts = [inst_tree[1][i]
+                                    for i, x in enumerate(values) if x in duplicates]
+                    incorrect_values = duplicates
+                    # avoid mentioning ifcopenshell.entity_instance twice in error message
+                    report_incorrect_insts = any(map_state(values, lambda v: do_try(
+                        lambda: isinstance(v, ifcopenshell.entity_instance), False)))
+                    errors.append(duplicate_value_error(inst, incorrect_values, attribute,
+                                incorrect_insts, report_incorrect_insts))
 
     handle_errors(context, errors)
 

291f141bf822efa12c0dbd8f860632bc4f8e7985
Geert Hesselink <geert.hess@gmail.com>
2023-02-06 23:06:57 +0100

    fix merge errors

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -684,8 +684,10 @@ def step_impl(context, constraint, num=None):
 
     within_model = getattr(context, 'within_model', False)
 
-    if constraint.startswith('be ') or constraint.startswith('in '):
-        constraint = constraint[3:]
+    #to account for order-dependency of removing characters from constraint
+    for startswith, length in itertools.chain.from_iterable(itertools.permutations([('not ', 4), ('be ', 3), ('in ', 3)])):
+        if constraint.startswith(startswith):
+            constraint = constraint[length:]
 
     if getattr(context, 'applicable', True):
         stack_tree = list(

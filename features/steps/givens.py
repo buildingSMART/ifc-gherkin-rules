d8e124f7aa49ac2d1700eda321e577f131ec2681
JTurbakiewicz <turbakiewicz.jakub@gmail.com>
2023-02-10 00:48:14 +0100

    Separate steps.py file

---
+++ features/steps/givens.py
@@ -0,0 +1,122 @@
+import ast
+import functools
+import re
+
+from behave import *
+from utils import *
+from errors import *
+
+@given('Its attribute {attribute}')
+def step_impl(context, attribute):
+    context._push()
+    context.instances = map_state(context.instances, lambda i: getattr(i, attribute, None))
+    setattr(context, 'attribute', attribute)
+
+
+@given("{attribute} = {value}")
+def step_impl(context, attribute, value):
+    value = ast.literal_eval(value)
+    context.instances = list(
+        filter(lambda inst: getattr(inst, attribute, True) == value, context.instances)
+    )
+
+
+@given("The element {relationship_type} an {entity}")
+def step_impl(context, relationship_type, entity):
+    reltype_to_extr = {'nests': {'attribute': 'Nests', 'object_placement': 'RelatingObject'},
+                       'is nested by': {'attribute': 'IsNestedBy', 'object_placement': 'RelatedObjects'}}
+    assert relationship_type in reltype_to_extr
+    extr = reltype_to_extr[relationship_type]
+    context.instances = list(filter(lambda inst: do_try(lambda: getattr(getattr(inst, extr['attribute'])[0], extr['object_placement']).is_a(entity), False), context.instances))
+
+
+@given('A file with {field} "{values}"')
+def step_impl(context, field, values):
+    values = strip_split(values, strp='"', splt=' or ')
+    if field == "Model View Definition":
+        conditional_lowercase = lambda s: s.lower() if s else None
+        applicable = conditional_lowercase(get_mvd(context.model)) in values
+    elif field == "Schema Identifier":
+        applicable = context.model.schema.lower() in values
+    else:
+        raise NotImplementedError(f'A file with "{field}" is not implemented')
+
+    context.applicable = getattr(context, 'applicable', True) and applicable
+
+
+@given('Its values')
+@given('Its values excluding {excluding}')
+def step_impl(context, excluding=()):
+    context._push()
+    context.instances = map_state(context.instances, lambda inst: do_try(
+        lambda: inst.get_info(recursive=True, include_identifier=False, ignore=excluding), None))
+
+
+@given('A relationship {relationship} from {entity} to {other_entity}')
+def step_impl(context, entity, other_entity, relationship):
+    instances = []
+    relationships = context.model.by_type(relationship)
+
+    filename_related_attr_matrix = get_abs_path(f"resources/**/related_entity_attributes.csv")
+    filename_relating_attr_matrix = get_abs_path(f"resources/**/relating_entity_attributes.csv")
+    related_attr_matrix = get_csv(filename_related_attr_matrix, return_type='dict')[0]
+    relating_attr_matrix = get_csv(filename_relating_attr_matrix, return_type='dict')[0]
+    for rel in relationships:
+        regex = re.compile(r'([0-9]+=)([A-Za-z0-9]+)\(')
+        relationships_str = regex.search(str(rel)).group(2)
+        relationship_relating_attr = relating_attr_matrix.get(relationships_str)
+        relationship_related_attr = related_attr_matrix.get(relationships_str)
+        if getattr(rel, relationship_relating_attr).is_a(other_entity):
+            try:  # check if the related attribute returns a tuple/list or just a single instance
+                iter(getattr(rel, relationship_related_attr))
+                related_objects = getattr(rel, relationship_related_attr)
+            except TypeError:
+                related_objects = tuple(getattr(rel, relationship_related_attr))
+            for obj in related_objects:
+                if obj.is_a(entity):
+                    instances.append(obj)
+    context.instances = instances
+
+
+@given('{attr} forms {closed_or_open} curve')
+def step_impl(context, attr, closed_or_open):
+    assert closed_or_open in ('a closed', 'an open')
+    should_be_closed = closed_or_open == 'a closed'
+    if attr == 'It':  # if a pronoun is used instances are filtered based on previously established context
+        instances = context.instances
+    else:  # if a specific entity is used instances are filtered based on the ifc model
+        instances = map(operator.attrgetter(attr), context.instances)
+
+    are_closed = []
+    for instance in instances:
+        are_closed.append(is_closed(context, instance))
+
+    context.instances = list(
+        map(operator.itemgetter(0), filter(lambda pair: pair[1] == should_be_closed, zip(context.instances, are_closed)))
+    )
+
+
+@given('The {representation_id} shape representation has RepresentationType "{representation_type}"')
+def step_impl(context, representation_id, representation_type):
+    context.instances = list(filter(None, list(map(lambda i: instance_getter(i, representation_id, representation_type), context.instances))))
+
+
+@given("An {entity_opt_stmt}")
+@given("All {insts} of {entity_opt_stmt}")
+def step_impl(context, entity_opt_stmt, insts=False):
+    within_model = (insts == 'instances')  # True for given statement containing {insts}
+
+    entity2 = pyparsing.Word(pyparsing.alphas)('entity')
+    sub_stmts = ['with subtypes', 'without subtypes', pyparsing.LineEnd()]
+    incl_sub_stmt = functools.reduce(operator.or_, [rtrn_pyparse_obj(i) for i in sub_stmts])('include_subtypes')
+    grammar = entity2 + incl_sub_stmt
+    parse = grammar.parseString(entity_opt_stmt)
+    entity = parse['entity']
+    include_subtypes = do_try(lambda: not 'without' in parse['include_subtypes'], True)
+
+    try:
+        context.instances = context.model.by_type(entity, include_subtypes)
+    except:
+        context.instances = []
+
+    context.within_model = getattr(context, 'within_model', True) and within_model
---
+++ features/steps/utils.py
@@ -0,0 +1,265 @@
+import os
+import json
+import operator
+import csv
+import glob
+import ifcopenshell
+import math
+import pyparsing
+
+from pathlib import Path
+
+def is_a(s):
+    return lambda inst: inst.is_a(s)
+
+
+def get_edges(file, inst, sequence_type=frozenset, oriented=False):
+    edge_type = tuple if oriented else frozenset
+
+    def inner():
+        if inst.is_a("IfcConnectedFaceSet"):
+            deps = file.traverse(inst)
+            loops = filter(is_a("IfcPolyLoop"), deps)
+            for lp in loops:
+                coords = list(map(operator.attrgetter("Coordinates"), lp.Polygon))
+                shifted = coords[1:] + [coords[0]]
+                yield from map(edge_type, zip(coords, shifted))
+            edges = filter(is_a("IfcOrientedEdge"), deps)
+            for ed in edges:
+                # @todo take into account edge geometry
+                # edge_geom = ed[2].EdgeGeometry.get_info(recursive=True, include_identifier=False)
+                coords = [
+                    ed.EdgeElement.EdgeStart.VertexGeometry.Coordinates,
+                    ed.EdgeElement.EdgeEnd.VertexGeometry.Coordinates,
+                ]
+                # @todo verify:
+                # if not ed.EdgeElement.SameSense:
+                #     coords.reverse()
+                if not ed.Orientation:
+                    coords.reverse()
+                yield edge_type(coords)
+        elif inst.is_a("IfcTriangulatedFaceSet"):
+            # @nb to decide: should we return index pairs, or coordinate pairs here?
+            coords = inst.Coordinates.CoordList
+            for idx in inst.CoordIndex:
+                for ij in zip(range(3), ((x + 1) % 3 for x in range(3))):
+                    yield edge_type(coords[idx[x] - 1] for x in ij)
+        elif inst.is_a("IfcPolygonalFaceSet"):
+            coords = inst.Coordinates.CoordList
+            for f in inst.Faces:
+                def emit(loop):
+                    fcoords = list(map(lambda i: coords[i - 1], loop))
+                    shifted = fcoords[1:] + [fcoords[0]]
+                    return map(edge_type, zip(fcoords, shifted))
+
+                yield from emit(f.CoordIndex)
+
+                if f.is_a("IfcIndexedPolygonalFaceWithVoids"):
+                    for inner in f.InnerCoordIndices:
+                        yield from emit(inner)
+        else:
+            raise NotImplementedError(f"get_edges({inst.is_a()})")
+
+    return sequence_type(inner())
+
+
+def condition(inst, representation_id, representation_type):
+    def is_valid(inst, representation_id, representation_type):
+        representation_type = list(map(lambda s: s.strip(" ").strip("\""), representation_type.split(",")))
+        return any([repr.RepresentationIdentifier in representation_id and repr.RepresentationType in representation_type for repr in do_try(lambda: inst.Representation.Representations, [])])
+
+    if is_valid(inst, representation_id, representation_type):
+        return any([repr.RepresentationIdentifier == representation_id and repr.RepresentationType in representation_type for repr in do_try(lambda: inst.Representation.Representations, [])])
+
+
+def instance_getter(i, representation_id, representation_type, negative=False):
+    if negative:
+        if not condition(i, representation_id, representation_type):
+            return i
+    else:
+        if condition(i, representation_id, representation_type):
+            return i
+
+
+def strip_split(stmt, strp=' ', splt=','):
+    return list(
+        map(lambda s: s.strip(strp), stmt.lower().split(splt))
+    )
+
+
+def include_subtypes(stmt):
+    # todo replace by pyparsing?
+    stmt = strip_split(stmt, strp='[]', splt=' ')
+    excluding_statements = {'without', 'not', 'excluding', 'no'}
+    return not set(stmt).intersection(set(excluding_statements))
+
+
+def map_state(values, fn):
+    if isinstance(values, (tuple, list)):
+        return type(values)(map_state(v, fn) for v in values)
+    else:
+        return fn(values)
+
+
+def rtrn_pyparse_obj(i):
+    if isinstance(i, (pyparsing.core.LineEnd, pyparsing.core.NotAny)):
+        return i
+    elif isinstance(i, str):
+        return pyparsing.CaselessKeyword(i)
+
+
+def handle_errors(context, errors):
+    error_formatter = (lambda dc: json.dumps(asdict(dc), default=tuple)) if context.config.format == ["json"] else str
+    assert not errors, "Errors occured:\n{}".format(
+        "\n".join(map(error_formatter, errors))
+    )
+
+
+def map_state(values, fn):
+    if isinstance(values, (tuple, list)):
+        return type(values)(map_state(v, fn) for v in values)
+    else:
+        return fn(values)
+
+
+def unpack_sequence_of_entities(instances):
+    # in case of [[inst1, inst2], [inst3, inst4]]
+    return [do_try(lambda: unpack_tuple(inst), None) for inst in instances]
+
+
+def unpack_tuple(tup):
+    for item in tup:
+        if isinstance(item, tuple):
+            unpack_tuple(item)
+        else:
+            return item
+
+def instance_converter(kv_pairs):
+    def c(v):
+        if isinstance(v, ifcopenshell.entity_instance):
+            return str(v)
+        else:
+            return v
+
+    return {k: c(v) for k, v in kv_pairs}
+
+
+def get_mvd(ifc_file):
+    try:
+        detected_mvd = ifc_file.header.file_description.description[0].split(" ", 1)[1]
+        detected_mvd = detected_mvd[1:-1]
+    except:
+        detected_mvd = None
+    return detected_mvd
+
+
+def get_inst_attributes(dc):
+    if hasattr(dc, 'inst'):
+        yield 'inst_guid', getattr(dc.inst, 'GlobalId', None)
+        yield 'inst_type', dc.inst.is_a()
+        yield 'inst_id', dc.inst.id()
+
+
+def stmt_to_op(statement):
+    statement = statement.replace('is', '').strip()
+    stmt_to_op = {
+        '': operator.eq,  # a == b
+        "equal to": operator.eq,  # a == b
+        "exactly": operator.eq,  # a == b
+        "not": operator.ne,  # a != b
+        "at least": operator.ge,  # a >= b
+        "more than": operator.gt,  # a > b
+        "at most": operator.le,  # a <= b
+        "less than": operator.lt  # a < b
+    }
+    assert statement in stmt_to_op
+    return stmt_to_op[statement]
+
+
+# @note dataclasses.asdict used deepcopy() which doesn't work on entity instance
+asdict = lambda dc: dict(instance_converter(dc.__dict__.items()), message=str(dc), **dict(get_inst_attributes(dc)))
+
+
+def fmt(x):
+    if isinstance(x, frozenset) and len(x) == 2 and set(map(type, x)) == {tuple}:
+        return "{} -- {}".format(*x)
+    elif isinstance(x, tuple) and len(x) == 2 and set(map(type, x)) == {tuple}:
+        return "{} -> {}".format(*x)
+    else:
+        v = str(x)
+        if len(v) > 35:
+            return "...".join((v[:25], v[-7:]))
+        return v
+
+
+def do_try(fn, default=None):
+    try:
+        return fn()
+    except:
+        return default
+
+
+def get_abs_path(rel_path):
+    dir_name = os.path.dirname(__file__)
+    parent_path = Path(dir_name).parent
+    csv_path = do_try(lambda: glob.glob(os.path.join(parent_path, rel_path), recursive=True)[0])
+    return csv_path
+
+
+def get_csv(abs_path, return_type='list', newline='', delimiter=',', quotechar='|'):
+    with open(abs_path, newline=newline) as csvfile:
+        if return_type == 'dict':
+            reader = csv.DictReader(csvfile)
+        elif return_type == 'list':
+            reader = csv.reader(csvfile, delimiter=delimiter, quotechar=quotechar)
+        return [row for row in reader]
+
+
+def is_closed(context, instance):
+    entity_contexts = recurrently_get_entity_attr(context, instance, 'IfcRepresentation', 'ContextOfItems')
+    precision = get_precision_from_contexts(entity_contexts)
+    points_coordinates = get_points(instance)
+    return math.dist(points_coordinates[0], points_coordinates[-1]) < precision
+
+
+def get_points(inst, return_type='coord'):
+    if inst.is_a().startswith('IfcCartesianPointList'):
+        return inst.CoordList
+    elif inst.is_a('IfcPolyline'):
+        if return_type == 'coord':
+            return [p.Coordinates for p in inst.Points]
+        elif return_type == 'points':
+            return inst.Points
+    elif inst.is_a('IfcPolyLoop'):
+        if return_type == 'coord':
+            return [p.Coordinates for p in inst.Polygon]
+        elif return_type == 'points':
+            return inst.Polygon
+    else:
+        raise NotImplementedError(f'get_points() not implemented on {inst.is_a}')
+
+
+def recurrently_get_entity_attr(ifc_context, inst, entity_to_look_for, attr_to_get, attr_found=None):
+    if attr_found is None:
+        attr_found = set()
+    if inst.is_a(entity_to_look_for):
+        return getattr(inst, attr_to_get)
+    else:
+        for inv_item in ifc_context.model.get_inverse(inst):
+            if inv_item.is_a(entity_to_look_for):
+                attr_found.add((getattr(inv_item, attr_to_get)))
+            else:
+                recurrently_get_entity_attr(ifc_context, inv_item, entity_to_look_for, attr_to_get, attr_found)
+    return attr_found
+
+def get_precision_from_contexts(entity_contexts, func_to_return=max, default_precision=1e-05):
+    precisions = []
+    if not entity_contexts:
+        return default_precision
+    for entity_context in entity_contexts:
+        if entity_context.is_a('IfcGeometricRepresentationSubContext'):
+            precision = get_precision_from_contexts([entity_context.ParentContext])
+        elif entity_context.is_a('IfcGeometricRepresentationContext') and entity_context.Precision:
+            return entity_context.Precision
+        precisions.append(precision)
+    return func_to_return(precisions)

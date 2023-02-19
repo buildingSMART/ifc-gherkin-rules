963df87e87ae1137cc94a0692a250fbe88d244d4
JTurbakiewicz <turbakiewicz.jakub@gmail.com>
2023-02-19 19:26:41 +0100

    Parse code using ast module

--- features/resources/list_repository/_list_funcions.py
+++ features/resources/list_repository/_list_funcions.py
@@ -1,4 +1,5 @@
 if __name__ == "__main__":
+    import ast
     import glob
     import json
     import os
@@ -8,7 +9,7 @@ if __name__ == "__main__":
     parent_path = Path(curr_dir_name).parent.parent
     steps_path = os.path.join(parent_path, 'steps')
 
-    signatures = {'givens': '@given', 'thens': '@then', 'utils': 'def '}
+    signatures = {'givens': 'given', 'thens': 'then', 'utils': 'def'}
     skips = ['__init__.py']
 
     for directory, signature in signatures.items():
@@ -22,18 +23,16 @@ if __name__ == "__main__":
             base_path = os.path.basename(os.path.normpath(file_path))
             if base_path in skips:
                 continue
-            with open(file_path, "r") as f:
+            with open(file_path, 'r') as f:
+                tree = ast.parse(f.read())
                 functions[base_path] = []
-                file_lines = f.readlines()
-                for line in file_lines:
-                    if signature in line:
-                        line = line.replace(':\n', '')
-                        line = line.replace('def ', '')
-                        line = line.replace('"', '')
-                        line = line.replace("'", '')
-                        line = line.strip()
-                        functions[base_path].append(line)
+                for node in ast.walk(tree):
+                    if directory in ['givens', 'thens'] and isinstance(node, ast.FunctionDef) and node.decorator_list:
+                        for decorator in node.decorator_list:
+                            if signature == decorator.func.id:
+                                functions[base_path].append(decorator.args[0].value)
+                    elif directory in ['utils'] and isinstance(node, ast.FunctionDef):
+                        functions[base_path].append(node.name)
             functions[os.path.basename(os.path.normpath(file_path))].sort()
-
         with open(f'{directory}_functions.json', 'w') as f:
             json.dump(functions, f)

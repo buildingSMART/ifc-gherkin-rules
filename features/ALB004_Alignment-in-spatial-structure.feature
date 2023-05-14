12693bda75179bd1f82698af2c69074777119817
JTurbakiewicz <turbakiewicz.jakub@gmail.com>
2023-05-14 23:50:07 +0200

    Initial commit

---
+++ features/ALB004_Alignment-in-spatial-structure.feature
@@ -0,0 +1,11 @@
+@implementer-agreement
+@ALB
+Feature: ALB004 - Alignment in spatial structure
+The rule verifies, that each IfcAlignment is contained in an IfcProject, but not contained in spatial structure.
+
+  Scenario: Agreement on each IfcAlignment being contained in an IfcProject and not in spatial structure
+
+      Given A file with Schema Identifier "IFC4X3_TC1" or "IFC4X3_ADD1" or "IFC4X3"
+      And An IfcAlignment
+      Then Each IfcAlignment must be aggregated to IfcProject as per spatial_CompositionTable.csv
+      And Each IfcAlignment must not be directly contained in IfcSpatialElement
--- features/steps/thens/relations.py
+++ features/steps/thens/relations.py
@@ -1,7 +1,7 @@
 import errors as err
 
 from behave import *
-from utils import misc
+from utils import misc, system
 
 @then('Each {entity} {condition} be {directness} contained in {other_entity}')
 def step_impl(context, entity, condition, directness, other_entity):
@@ -36,10 +36,54 @@ def step_impl(context, entity, condition, directness, other_entity):
             directness_achieved = bool(common_directness)  # if there's a common value -> relationship achieved
             directness_expected = condition == 'must'  # check if relationship is expected
             if directness_achieved != directness_expected:
-                errors.append(err.InstanceStructureError(ent, [other_entity], 'contained', optional_values={'condition': condition, 'directness': directness}))
-
+                errors.append(err.InstanceStructureError(ent, [relating_spatial_element], 'contained', optional_values={'condition': condition, 'directness': directness}))
     misc.handle_errors(context, errors)
 
+@then('Each {entity} must be {relationship} {preposition} {other_entity} as per {table}')
+def step_impl(context, entity, relationship, preposition, other_entity, table):
+
+    stmt_to_op = {'aggregated': 'Decomposes'}
+    assert relationship in stmt_to_op
+
+    tbl_path = system.get_abs_path(f"resources/{table}")
+    tbl = system.get_csv(tbl_path, return_type='dict')
+
+    ent_tbl_header, relationship_tbl_header = list(tbl[0].keys())
+
+    aggregated_table = {}
+
+    for d in tbl:
+        applicable_entity = d[ent_tbl_header]
+        tbl_relationship_object = d[relationship_tbl_header]
+        if applicable_entity in aggregated_table:
+            aggregated_table[applicable_entity].append(tbl_relationship_object)
+        else:
+            aggregated_table[applicable_entity] = [tbl_relationship_object]
+    errors = []
+
+    if context.instances and getattr(context, 'applicable', True):
+        for ent in context.model.by_type(entity):
+            for applicable_entity in aggregated_table.keys(): # check which applicable entity the currently processed entity is (inheritance), e.g IfcRailway -> IfcFacility
+                if ent.is_a(applicable_entity):
+                    break # TODO - > is the hierarchy important?
+            expected_relationship_objects = aggregated_table[applicable_entity]
+
+            try:
+                relation = getattr(ent, stmt_to_op[relationship], True)[0]
+            except IndexError: # no relationship found for the entity
+                errors.append(err.InstanceStructureError(ent, [expected_relationship_objects], 'related to', optional_values={'condition': 'must'}))
+                continue
+            relationship_objects = getattr(relation, relationship_tbl_header, True)
+            if isinstance(relationship_objects, tuple):
+                for relationship_object in relationship_objects:
+                    is_correct = any(relationship_object.is_a(expected_relationship_object) for expected_relationship_object in expected_relationship_objects)
+                    if not is_correct:
+                        errors.append(err.InstanceStructureError(ent, [expected_relationship_objects], 'related to', optional_values={'condition': 'must'}))
+            else:
+                is_correct = any(relationship_objects.is_a(expected_relationship_object) for expected_relationship_object in expected_relationship_objects)
+                if not is_correct:
+                    errors.append(err.InstanceStructureError(ent, [expected_relationship_objects], 'related to', optional_values={'condition': 'must'}))
+    misc.handle_errors(context, errors)
 
 @then('The {related} must be assigned to the {relating} if {other_entity} {condition} present')
 def step_impl(context, related, relating, other_entity, condition):

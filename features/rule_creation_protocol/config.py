85852823e560fd83cbd2325e7bab16cde0ccac82
Geert Hesselink <geert.hess@gmail.com>
2023-07-28 19:31:04 +0200

    update depreciated pydantic config

--- features/rule_creation_protocol/config.py
+++ features/rule_creation_protocol/config.py
@@ -1,9 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 class ConfiguredBaseModel(BaseModel):
     """Base Pydantic model with assignment validation enabled.
 
     Serves as a base class for Pydantic models, enabling validation when instance values are changed.
     """
-    class Config:
-        validate_assignment = True
+    model_config = ConfigDict(validate_assignment=True)

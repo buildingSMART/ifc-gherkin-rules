3ef342c0db5f888a4c5ab5656765145ae38273eb
Geert Hesselink <54070862+Ghesselink@users.noreply.github.com>
2023-09-29 21:14:35 +0200

    Remove uncommented code

--- features/steps/utils/misc.py
+++ features/steps/utils/misc.py
@@ -32,18 +32,6 @@ def fmt(x):
             return "...".join((v[:25], v[-7:]))
         return v
 
-# def handle_errors(fn):
-#     def inner(*args, **kwargs):
-#         err.generate_error_message(*args, list(fn(*args, **kwargs))) # context is always *args[0]
-#     return inner
-
-# def generate_error_message(context, errors):
-#     error_formatter = (lambda dc: json.dumps(asdict(dc), default=tuple)) if context.config.format == ["json"] else str
-#     assert not errors, "Errors occured:\n{}".format(
-#         "\n".join(map(error_formatter, errors))
-#     )
-
-
 def include_subtypes(stmt):
     # todo replace by pyparsing?
     stmt = strip_split(stmt, strp='[]', splt=' ')

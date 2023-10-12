af638dc88756a45cc1fcfa6a73838664150e0d17
Geert Hesselink <54070862+Ghesselink@users.noreply.github.com>
2023-10-12 09:43:57 +0200

    fix testing disabled rules with pytest (#95)
    * fix testing disabled rules with pytest
    
    * Fixed list comprehension reference error
    
    * exclude disabled testfiles

--- test/test_main.py
+++ test/test_main.py
@@ -15,6 +15,23 @@ except ImportError:
 rule_code_pattern = re.compile(r"^[a-zA-Z]{3}\d{3}$")
 rule_codes = list(filter(lambda arg: rule_code_pattern.match(arg), sys.argv[1:]))
 
+    
+def exclude_disabled_rules(filepath):
+    tags = []
+    
+    with open(filepath, 'r') as file:
+        for line in file:
+            stripped_line = line.strip()  # Remove any leading or trailing whitespace
+            if stripped_line.startswith("@"):
+                tags_on_line = stripped_line.split()
+                tags.extend(tags_on_line)
+    return '@disabled' in tags
+
+def rule_disabled(code: str) -> bool:
+    feature_paths = glob.glob(os.path.join(os.path.dirname(__file__), "../features/**/*.feature"), recursive=True)
+    return next((exclude_disabled_rules(path) for path in feature_paths if os.path.basename(path).lower().startswith(code)), False)
+
+
 def get_test_files():
     """
     Option -> Example
@@ -33,6 +50,9 @@ def get_test_files():
         paths = glob.glob(os.path.join(os.path.dirname(__file__), "files/", code.lower(), "*.ifc"))
         if not paths:
             print(f"No IFC files were found for the following rule code: {code}. Please provide test files or verify the input.")
+        elif rule_disabled(code):
+            print(f"The rule identified by code '{code}' is currently marked as 'disabled'. Any associated test files will not be taken into consideration")
+            continue
         test_files.extend(paths)
 
     file_pattern =  r".*\.ifc(\')?$" #matches ".ifc" and "ifc'"
@@ -44,22 +64,24 @@ def get_test_files():
 
 @pytest.mark.parametrize("filename", get_test_files())
 def test_invocation(filename):
-    results = list(run(filename))
+    gherkin_results = list(run(filename))
     base = os.path.basename(filename)
     # if base.startswith("pass-"):
-    results = [result for result in results if result[4] != 'Rule disabled']
+    results = [result for result in gherkin_results if result[4] != 'Rule disabled']
     results = [result for result in results if 'Rule passed' not in result[4]]
     print()
     print(base)
     print()
     print(f"{len(results)} errors")
-    if results:
+
+    if gherkin_results:
         print(tabulate.tabulate(
-            [[c or '' for c in r] for r in results],
-            maxcolwidths=[30] * len(results[0]),
+            [[c or '' for c in r] for r in gherkin_results],
+            maxcolwidths=[30] * len(gherkin_results[0]),
             tablefmt="simple_grid"
         ))
-    if base.startswith("fail-"):
+
+    if base.startswith("fail-") and not any(description == 'Rule disabled' for description in [result[4] for result in gherkin_results]):
         assert len(results) > 0
     elif base.startswith("pass-"):
         assert len(results) == 0

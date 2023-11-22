5f957742af64a215ee4af3114f96c567009ffb3b
Ghesselink <geert.hess@gmail.com>
2023-11-22 21:41:37 +0100

    modify gem002 step_impl :  there must be {representation_id} shape representation

--- features/steps/thens/existance.py
+++ features/steps/thens/existance.py
@@ -3,18 +3,14 @@ import operator
 
 from behave import *
 from utils import misc
+from validation_handling import validate_step
 
-@then("There must be one {representation_id} shape representation")
-@err.handle_errors
-def step_impl(context, representation_id):
-    if context.instances:
-        for inst in context.instances:
-            if inst.Representation:
-                present = representation_id in map(operator.attrgetter('RepresentationIdentifier'), inst.Representation.Representations)
-                if not present:
-                    yield(err.RepresentationShapeError(False, inst, representation_id))
-                elif context.error_on_passed_rule:
-                    yield(err.RuleSuccessInst(True, inst))
+@validate_step("There must be one {representation_id} shape representation")
+def step_impl(context, inst, representation_id):
+    if inst.Representation:
+        present = representation_id in map(operator.attrgetter('RepresentationIdentifier'), inst.Representation.Representations)
+        if not present:
+            yield(err.RepresentationShapeError(False, inst, representation_id))
 
 
 @then('There must be {constraint} {num:d} instance(s) of {entity}')

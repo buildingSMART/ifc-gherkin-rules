74a8d0eed93ceb287998fa754d58cc3e6e88852a
Ghesselink <geert.hess@gmail.com>
2023-11-22 22:05:42 +0100

    change step_impl for ojp001 and alignment nesting alb rules

--- features/steps/thens/attributes.py
+++ features/steps/thens/attributes.py
@@ -5,45 +5,6 @@ from behave import *
 from utils import ifc, misc, system
 from validation_handling import validate_step
 
-# @then('The {entity} attribute must point to the {other_entity} of the container element established with {relationship} relationship')
-# @err.handle_errors
-# def step_impl(context, entity, other_entity, relationship):
-#     if getattr(context, 'applicable', True):
-#         errors = []
-#         filename_related_attr_matrix = system.get_abs_path(f"resources/**/related_entity_attributes.csv")
-#         filename_relating_attr_matrix = system.get_abs_path(f"resources/**/relating_entity_attributes.csv")
-#         related_attr_matrix = system.get_csv(filename_related_attr_matrix, return_type='dict')[0]
-#         relating_attr_matrix = system.get_csv(filename_relating_attr_matrix, return_type='dict')[0]
-
-#         relationship_relating_attr = relating_attr_matrix.get(relationship)
-#         relationship_related_attr = related_attr_matrix.get(relationship)
-#         relationships = context.model.by_type(relationship)
-
-#         for rel in relationships:
-#             try:  # check if the related attribute returns a tuple/list or just a single instance
-#                 iter(getattr(rel, relationship_related_attr))
-#                 related_objects = getattr(rel, relationship_related_attr)
-#             except TypeError:
-#                 related_objects = tuple(getattr(rel, relationship_related_attr))
-#             for related_object in related_objects:
-#                 if related_object not in context.instances:
-#                     continue
-#                 related_obj_placement = related_object.ObjectPlacement
-#                 entity_obj_placement_rel = related_obj_placement.PlacementRelTo
-#                 relating_object = getattr(rel, relationship_relating_attr)
-#                 relating_obj_placement = relating_object.ObjectPlacement
-#                 try:
-#                     entity_obj_placement_rel = related_obj_placement.PlacementRelTo
-#                     is_correct = relating_obj_placement == entity_obj_placement_rel
-#                 except AttributeError:
-#                     is_correct = False
-#                 if not entity_obj_placement_rel:
-#                     entity_obj_placement_rel = 'Not found'
-#                 if not is_correct:
-#                     yield(err.InstancePlacementError(False, related_object, '', relating_object, relationship, relating_obj_placement, entity_obj_placement_rel))
-#                 elif context.error_on_passed_rule:
-#                     yield(err.RuleSuccess(True, relating_object))
-
 
 @validate_step('The {entity} attribute must point to the {other_entity} of the container element established with {relationship} relationship')
 def step_impl(context, inst, entity, other_entity, relationship):
@@ -62,29 +23,9 @@ def step_impl(context, inst, entity, other_entity, relationship):
             relating_object = getattr(rel, relationship_relating_attr)
 
             relating_obj_placement = relating_object.ObjectPlacement
-            
-            # try:
-            #     entity_obj_placement_rel = related_obj_placement.PlacementRelTo
-            #     is_correct = relating_obj_placement == entity_obj_placement_rel
-            # except AttributeError:
-            #     is_correct = False
-            # if not entity_obj_placement_rel:
-            #     entity_obj_placement_rel = 'Not found'
-            # if not is_correct:
-            #     yield(err.InstancePlacementError(False, related_object, '', relating_object, relationship, relating_obj_placement, entity_obj_placement_rel))
-
-            relating_obj_placement = relating_object.ObjectPlacement
             entity_obj_placement_rel = misc.do_try(lambda: related_obj_placement.PlacementRelTo, 'Not found')
             if relating_obj_placement != entity_obj_placement_rel:
                 yield(err.InstancePlacementError(False, related_object, '', relating_object, relationship, relating_obj_placement, entity_obj_placement_rel))
-
-            # relating_obj_placement = relating_object.ObjectPlacement
-            # entity_obj_placement_rel = misc.do_try(lambda: related_obj_placement.PlacementRelTo, 'Not found')
-
-            # is_correct = relating_obj_placement == entity_obj_placement_rel if entity_obj_placement_rel != 'Not found' else False
-
-            # if not is_correct:
-            #     yield(err.InstancePlacementError(False, related_object, '', relating_object, relationship, relating_obj_placement, entity_obj_placement_rel))
 
 
 @validate_step('The {representation_id} shape representation has RepresentationType "{representation_type}"')

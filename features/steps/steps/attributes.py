24f5b242ffd54b1fe3b14b1dea9113f4a51971d2
Ghesselink <geert.hess@gmail.com>
2023-12-20 20:40:53 +0100

    remove outcommented code

--- features/steps/steps/attributes.py
+++ features/steps/steps/attributes.py
@@ -2,14 +2,6 @@ from validation_handling import validate_step, StepResult
 from utils import ifc
 from behave import *
 
-# @validate_step('The {representation_id} shape representation has RepresentationType "{representation_type}"')
-# def step_impl(context, inst, representation_id, representation_type):
-#     if ifc.instance_getter(inst, representation_id, representation_type, 1):
-#         yield StepResult(expected=representation_type, observed=None)
-
-# @given('The {representation_id} shape representation has RepresentationType "{representation_type}"')
-# def step_impl(context, representation_id, representation_type):
-#     context.instances = list(filter(None, list(map(lambda i: ifc.instance_getter(i, representation_id, representation_type), context.instances))))
 
 @validate_step('The {representation_id} shape representation has RepresentationType "{representation_type}"')
 def step_impl(context, **kwargs):

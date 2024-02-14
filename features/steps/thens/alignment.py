2932aa7c2ccfc0579ce5a0e36813f9aa8017ddf7
Scott Lecher <civilx64@gmail.com>
2024-02-13 20:26:35 -0500

    Add ALA002
    - feature file
    - step implementation
    - test data

--- features/steps/thens/alignment.py
+++ features/steps/thens/alignment.py
@@ -51,6 +51,23 @@ def is_3d(entity: ifcopenshell.entity_instance) -> bool:
         )
 
 
+def count_segments(logic, representation):
+    """
+    Used in ALA002 to return count of segments for business logic
+    and geometry representation.
+    """
+    try:
+        logic_count = len(logic.segments)
+    except AttributeError:
+        logic_count = None
+    try:
+        rep_count = len(representation.segments)
+    except AttributeError:
+        rep_count = None
+
+    return logic_count, rep_count
+
+
 @gherkin_ifc.step('A representation by {ifc_rep_criteria} requires the {ifc_layout_criteria} in the business logic')
 def step_impl(context, inst, ifc_rep_criteria, ifc_layout_criteria):
     for align_ent in context.instances:
@@ -63,8 +80,8 @@ def step_impl(context, inst, ifc_rep_criteria, ifc_layout_criteria):
                                                 severity=OutcomeSeverity.ERROR)
 
             case ("IfcGradientCurve", "presence of IfcAlignmentVertical"):
-                if align.gradient_curves is not None:
-                    if align.verticals is None:
+                if align.gradient_curve is not None:
+                    if align.vertical is None:
                         yield ValidationOutcome(inst=inst, expected=ifc_layout_criteria, observed=None,
                                                 severity=OutcomeSeverity.ERROR)
 
@@ -73,7 +90,7 @@ def step_impl(context, inst, ifc_rep_criteria, ifc_layout_criteria):
                 for shape_rep in product_rep.Representations:
                     for item in shape_rep.Items:
                         if (item.is_a().upper() == "IFCINDEXEDPOLYCURVE") and (is_3d(item)):
-                            if align.verticals is None:
+                            if align.vertical is None:
                                 yield ValidationOutcome(inst=inst, expected=ifc_layout_criteria, observed=None,
                                                         severity=OutcomeSeverity.ERROR)
 
@@ -82,7 +99,7 @@ def step_impl(context, inst, ifc_rep_criteria, ifc_layout_criteria):
                 for shape_rep in product_rep.Representations:
                     for item in shape_rep.Items:
                         if (item.is_a().upper() == "IFCPOLYLINE") and (is_3d(item)):
-                            if align.verticals is None:
+                            if align.vertical is None:
                                 yield ValidationOutcome(inst=inst, expected=ifc_layout_criteria, observed=None,
                                                         severity=OutcomeSeverity.ERROR)
 
@@ -93,7 +110,7 @@ def step_impl(context, inst, ifc_rep_criteria, ifc_layout_criteria):
                         if (item.is_a().upper() == "IFCCOMPOSITECURVE") and (
                                 shape_rep.RepresentationIdentifier == "Axis"):
                             observed = list()
-                            if align.verticals is not None:
+                            if align.vertical is not None:
                                 observed.append("IfcAlignmentVertical")
                             if align.cant is not None:
                                 observed.append("IfcAlignmentCant")
@@ -110,3 +127,43 @@ def step_impl(context, inst, ifc_rep_criteria, ifc_layout_criteria):
                                 yield ValidationOutcome(inst=inst, expected=ifc_layout_criteria,
                                                         observed="IfcAlignmentCant",
                                                         severity=OutcomeSeverity.ERROR)
+
+
+@gherkin_ifc.step(
+    'The alignment\'s {layout_type} layout must have the same number of segments as the shape representation')
+def step_impl(context, inst, layout_type):
+    for layout_ent in context.instances:
+        for rel in layout_ent.Nests:
+            ent = rel.RelatingObject
+            if ent.is_a() == "IfcAlignment":
+                align = ifc43x_alignment_validation.entities.Alignment().from_entity(ent)
+
+                match layout_type:
+                    case "horizontal":
+                        logic_count, rep_count = count_segments(
+                            logic=align.horizontal,
+                            representation=align.composite_curve,
+                        )
+                    case "vertical":
+                        logic_count, rep_count = count_segments(
+                            logic=align.vertical,
+                            representation=align.gradient_curve,
+                        )
+                    case "cant":
+                        logic_count, rep_count = count_segments(
+                            logic=align.cant,
+                            representation=align.segmented_reference_curve,
+                        )
+                    case _:
+                        msg = f"Invalid value '{layout_type}'.  Should be 'horizontal', 'vertical', or 'cant'."
+                        raise NameError(msg)
+
+                if logic_count != rep_count:
+                    # account for alignment that has business logic only
+                    # or contains representation only
+                    logic_only = (logic_count is not None) and (rep_count is None)
+                    rep_only = (logic_count is None) and (rep_count is not None)
+                    if not (logic_only or rep_only):
+                        yield ValidationOutcome(inst=inst, expected="same count of segments",
+                                                observed="differing count of segments",
+                                                severity=OutcomeSeverity.ERROR)

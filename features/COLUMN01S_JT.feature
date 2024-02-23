87c56bee0d8959353b9d2203e341978dca36e48d
JTurbakiewicz <turbakiewicz.jakub@gmail.com>
2024-02-23 01:29:28 +0100

    Add Spatial Decomposition

--- features/COLUMN01S_JT.feature
+++ features/COLUMN01S_JT.feature
@@ -10,3 +10,10 @@ Feature: COLUMN01S
 
     Then It must be assigned to exact IfcBuilding with parameter Name equal to ColumnBuilding_1
     Then The value of attribute Elevation must be 0.0
+
+  Scenario: Project
+
+    Given An IfcProject
+    Given Name = 'IFC4RV_Column_01S'
+
+    Then It must be related to IfcSite with attribute Name equal to ColumnSite_1 through Spatial Decomposition
--- features/steps/certification_implementation/jakub.py
+++ features/steps/certification_implementation/jakub.py
@@ -1,2 +1,33 @@
 from validation_handling import gherkin_ifc
 from behave import *
+from . import ValidationOutcome, OutcomeSeverity
+
+# @gherkin_ifc.step("Its IfcLocalPlacement")
+# def step_impl(context, inst):
+#     print(inst)
+#     ins_plc = inst.ObjectPlacement
+#     print(ins_plc)
+#     print(dir(ins_plc))
+#     # print('PlacementRelTo', 'PlacesObject', 'ReferencedByPlacements', 'RelativePlacement')
+#     print(ins_plc.PlacementRelTo.PlacementRelTo.PlacementRelTo.PlacementRelTo)
+#
+#     assert clause in ('including', 'excluding')
+#
+#
+# # def step_impl(context, inst, entity, other_entity):
+# #     if not misc.do_try(lambda: inst.ObjectPlacement.is_a(other_entity), False):
+# #         yield ValidationOutcome(inst=inst, expected=other_entity, observed=inst.ObjectPlacement, severity=OutcomeSeverity.ERROR)
+
+@gherkin_ifc.step('It must be related to {entity} with attribute {attribute} equal to {value} through Spatial Decomposition')
+def step_impl(context, inst, entity, attribute, value):
+    for rel in inst.IsDecomposedBy:
+        related_objects = rel.RelatedObjects
+
+        correct = False
+
+        for related_object in related_objects:
+            if related_object.is_a(entity) and getattr(related_object, attribute, 'Attribute not found') == value:
+                correct = True
+
+        if not correct:
+            yield ValidationOutcome(inst=inst, expected=None, observed=None, severity=OutcomeSeverity.ERROR)

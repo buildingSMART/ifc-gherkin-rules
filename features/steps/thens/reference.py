bb26475c5b539a915289404c3762ee5055d26c99
Ghesselink <geert.hess@gmail.com>
2024-04-25 10:00:08 +0100

    fix/val-456-gem111-coordinates

--- features/steps/thens/reference.py
+++ features/steps/thens/reference.py
@@ -20,4 +20,12 @@ def step_impl(context, inst):
     inst = misc.recursive_unpack_value(inst)
     points = geometry.get_points(inst, return_type='points')
     if points[0] != points[-1]:
-        yield ValidationOutcome(inst=inst, observed=[points[0], points[-1]], severity=OutcomeSeverity.ERROR)
+        def get_coordinates(point):
+            return getattr(point, 'Coordinates', point)
+
+        yield ValidationOutcome(
+            inst=inst,
+            observed=[get_coordinates(points[0]), get_coordinates(points[1])],
+            severity=OutcomeSeverity.ERROR
+        )
+        # yield ValidationOutcome(inst=inst, observed=[getattr(points[0], 'Coordinates', points[0]), getattr(points[1], 'Coordinates', points[1])], severity=OutcomeSeverity.ERROR)

b6c1e86f56078287d580944115267f59a724d772
Geert Hesselink <geert.hess@gmail.com>
2024-06-05 17:55:05 +0200

    add clarifications for including subtypes in check_entity_type function

--- features/steps/givens/attributes.py
+++ features/steps/givens/attributes.py
@@ -30,7 +30,19 @@ register_type(equal_or_not_equal=TypeBuilder.make_enum({
     "is": ComparisonOperator.EQUAL,
 }))
 
-def check_entity_type(inst, entity_type, handling: SubTypeHandling):
+def check_entity_type(inst: ifcopenshell.entity_instance, entity_type: str, handling: SubTypeHandling) -> bool:
+    """
+    Check if the instance is of a specific entity type or its subtype.
+    INCLUDE will evaluate to True if inst is a subtype of entity_type while the second function for EXCLUDE will evaluate to True only for an exact type match
+
+    Parameters:
+    inst (ifcopenshell.entity_instance): The instance to check.
+    entity_type (str): The entity type to check against.
+    handling (SubTypeHandling): Determines whether to include subtypes or not.
+
+    Returns:
+    bool: True if the instance matches the entity type criteria, False otherwise.
+    """
     handling_functions = {
         SubTypeHandling.INCLUDE: lambda inst, entity_type: inst.is_a(entity_type),
         SubTypeHandling.EXCLUDE: lambda inst, entity_type: inst.is_a() == entity_type,

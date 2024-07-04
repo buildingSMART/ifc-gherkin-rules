a955145d03f08267719a3abf9020a8b15f7519d9
Thomas Krijnen <t.krijnen@gmail.com>
2024-07-04 10:00:13 +0200

    Fancify plotting

--- test/files/gem010/plot.py
+++ test/files/gem010/plot.py
@@ -1,4 +1,6 @@
+import os
 import sys, numpy, ifcopenshell, ifcopenshell.geom
+import matplotlib
 import matplotlib.pyplot as plt
 
 f = ifcopenshell.open(sys.argv[1])
@@ -14,7 +16,18 @@ vs = numpy.array(shp.verts).reshape((-1, 3))
 ed = numpy.array(shp.edges).reshape((-1, 2))[:,0]
 ed = numpy.concatenate((ed, [ed[0]]))
 cs = vs[ed][:,0:2]
+plt.figure()
+
+fn = os.path.basename(sys.argv[1])
+
+if fn.startswith('fail-'):
+    for axis in ['top','bottom','left','right']:
+        plt.gca().spines[axis].set_linewidth(4)
+        plt.gca().spines[axis].set_color('#ff0000')
+
 plt.plot(*cs.T)
+plt.title(' '.join(fn.split('-')[3:])[:-4].replace('_', ' '))
+
 try:
     plt.savefig(sys.argv[2])
 except:

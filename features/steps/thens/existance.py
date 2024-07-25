c1c20551db03ff4b84f83cdaea39fb9ef98c135d
Geert Hesselink <geert.hess@gmail.com>
2024-07-25 18:36:54 +0200

    feature/VAL-65 functional part commin step impl

--- features/steps/thens/existance.py
+++ features/steps/thens/existance.py
@@ -70,3 +70,9 @@ def step_impl(context, inst):
     if not any(recursive_flatten(inst)):
         expected = get_previous_step_before_assertion(context)
         yield ValidationOutcome(instance_id=inst, expected = expected, observed='Nonexistent', severity = OutcomeSeverity.ERROR)
+
+@gherkin_ifc.step("The IFC model contains information on {functional_part_description}")
+def step_impl(context, inst, functional_part_description):
+   # This rule is designed to always pass and is used solely to trigger the activation of the rule 
+    # if an instance linked to the functional part is present.
+    return []

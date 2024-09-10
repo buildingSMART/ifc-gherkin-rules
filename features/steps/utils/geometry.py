e906c60d892c3d4d885b4bf422d04cb0148540fa
Scott Lecher <civilx64@gmail.com>
2024-09-10 09:45:16 -0400

    - Edit unit test files to only fail a single scenario - Edit geometry calculations to accomodate multiple types of placements   (previously they assumed IfcAxis2Placement2D or IfcAxis2Placement3D,    which wouldn't accomodate other valid placement types such as IfcAxis2PlacementLinear)

--- features/steps/utils/geometry.py
+++ features/steps/utils/geometry.py
@@ -33,12 +33,12 @@ def get_edges(file, inst, sequence_type=frozenset, oriented=False):
                 for ed in bnd.Bound.EdgeList:
                     # @todo take into account edge geometry
                     # edge_geom = ed[2].EdgeGeometry.get_info(recursive=True, include_identifier=False)
-                    
+
                     coords = [
                         ed.EdgeElement.EdgeStart.VertexGeometry.Coordinates,
                         ed.EdgeElement.EdgeEnd.VertexGeometry.Coordinates,
                     ]
-                    
+
                     # @todo verify:
                     # @tfk: afaict, sense only affects the parametric space of the underlying curve,
                     #       not the topology of the begin/end vertices
@@ -106,6 +106,11 @@ def is_closed(context, instance):
 
 
 def evaluate_segment(segment: ifcopenshell.entity_instance, dist_along: float) -> np.ndarray:
+    """
+    Use ifcopenshell to calculate the 4x4 geometric transform at a point on an alignment segment
+    :param segment: The segment containing the point that we would like to
+    :param dist_along: The distance along this segment at the point of interest (point to be calculated)
+    """
     s = ifcos_geom.settings()
     pwf = wrapper.map_shape(s, segment.wrapped_data)
 
@@ -113,34 +118,63 @@ def evaluate_segment(segment: ifcopenshell.entity_instance, dist_along: float) -
 
     return np.array(prev_trans_matrix, dtype=np.float64).T
 
-def alignment_segment_positional_difference(length_unit_scale_factor, previous_segment, segment_to_analyze):
+
+def alignment_segment_positional_difference(
+        length_unit_scale_factor: float, previous_segment: ifcopenshell.entity_instance,
+        segment_to_analyze: ifcopenshell.entity_instance):
+    """
+    Use ifcopenshell to determine the difference in cartesian position between segments of an IfcAlignment.
+    The expected entity type is either `IfcCurveSegment` or `IfcCompositeCurveSegment`.
+
+    :param length_unit_scale_factor: Scale factor between the project units and metric units used internally by
+    ifcopenshell
+    :param previous_segment: The segment that precede the segment being analyzed.  The end point of this segment
+    will be determined via ifcopenshell geometry calculations.
+    :param segment_to_analyze: The segment under analysis.  The calculated end point of the previous segment will be
+    compared to the calculated start point of this segment.
+    """
 
     u = abs(previous_segment.SegmentLength.wrappedValue) * length_unit_scale_factor
     prev_end_transform = evaluate_segment(segment=previous_segment, dist_along=u)
+    current_start_transform = evaluate_segment(segment=segment_to_analyze, dist_along=0.0)
 
-    pX = prev_end_transform[3][0] / length_unit_scale_factor
-    pY = prev_end_transform[3][1] / length_unit_scale_factor
-    preceding_end = (pX, pY)
+    e0 = prev_end_transform[3][0] / length_unit_scale_factor
+    e1 = prev_end_transform[3][1] / length_unit_scale_factor
+    preceding_end = (e0, e1)
 
-    current_start = (
-        segment_to_analyze.Placement.Location.Coordinates[0],
-        segment_to_analyze.Placement.Location.Coordinates[1],
-    )
+    s0 = current_start_transform[3][0] / length_unit_scale_factor
+    s1 = current_start_transform[3][1] / length_unit_scale_factor
+    current_start = (s0, s1)
 
     return math.dist(preceding_end, current_start)
 
 
-def alignment_segment_angular_difference(length_unit_scale_factor, previous_segment, segment_to_analyze):
+def alignment_segment_angular_difference(
+        length_unit_scale_factor: float, previous_segment: ifcopenshell.entity_instance,
+        segment_to_analyze: ifcopenshell.entity_instance):
+    """
+    Use ifcopenshell to determine the difference in tangent direction angle between segments of an IfcAlignment.
+    The expected entity type is either `IfcCurveSegment` or `IfcCompositeCurveSegment`.
 
+    :param length_unit_scale_factor: Scale factor between the project units and metric units used internally by
+    ifcopenshell
+    :param previous_segment: The segment that precede the segment being analyzed.  The ending direction of this segment
+    will be determined via ifcopenshell geometry calculations.
+    :param segment_to_analyze: The segment under analysis.  The calculated ending direction of the previous segment
+    will be compared to the calculated starting direction of this segment.
+    """
     u = abs(float(previous_segment.SegmentLength.wrappedValue)) * length_unit_scale_factor
     prev_end_transform = evaluate_segment(segment=previous_segment, dist_along=u)
+    current_start_transform = evaluate_segment(segment=segment_to_analyze, dist_along=0.0)
 
     prev_i = prev_end_transform[0][0]
     prev_j = prev_end_transform[0][1]
     preceding_end_direction = math.atan2(prev_j, prev_i)
 
-    cur_i, cur_j = segment_to_analyze.Placement.RefDirection.DirectionRatios
-    current_start_direction = math.atan2(cur_j, cur_i)
+    curr_i = current_start_transform[0][0]
+    curr_j = current_start_transform[0][1]
+    current_start_direction = math.atan2(curr_j, curr_i)
+
     delta = abs(current_start_direction - preceding_end_direction)
 
     return delta

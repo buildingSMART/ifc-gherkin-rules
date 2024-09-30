4254e0410d9259d64284b83a1c3d37b0c03cb02c
Scott Lecher <civilx64@gmail.com>
2024-09-30 12:07:42 -0400

    fixes schema errors for BRP001 unit test suite
    IfcBuildingElementProxy.Name is a required attribute.
    Also adds the file name in the header.

--- test/files/brp001/generate.py
+++ test/files/brp001/generate.py
@@ -118,22 +118,27 @@ rect_near_colinear_cross = [
     [0.0, 2.0],
 ]
 
+
 is_valid = lambda shp: shp in (rect, parallel_rect, parallel_concave, rect_redundant, rect_near_colinear_cross)
 
 def polyloop(f, shp):
     ps = list(map(f.createIfcCartesianPoint, numpy.array(shp).tolist()))
     return f.createIfcPolyloop(ps)
 
+
 def indexed(f, shp):
     ps = f.createIfcCartesianPointList2D(shp)
     return f.createIfcIndexedPolyCurve(ps, [f.createIfcLineIndex([i + 1, (i + 1) % len(shp) + 1]) for i in range(len(shp))])
 
 # random (consistent) orthonormal matrix for rotating faces
+
 numpy.random.seed(42)
 Q, _ = numpy.linalg.qr(numpy.random.randn(3, 3))
 
+
 def transform_points(ps):
     return numpy.array([Q @ p for p in ps])
+
 
 def faceouterbound(permute):
     def inner(f, shp):
@@ -148,7 +153,8 @@ def faceouterbound(permute):
             )]
         )
     return inner
-    
+
+
 def faceinnerbound(permute):
     def inner(f, shp):
         arr = numpy.array(shp)
@@ -170,7 +176,8 @@ def faceinnerbound(permute):
             )]
         )
     return inner
-        
+
+
 def main():
     def process(fni, shp):
         functions = [
@@ -189,12 +196,16 @@ def main():
         shell = f.createIfcShellBasedSurfaceModel([f.createIfcOpenShell([fn(permute)(f, shp)])])
         rep = f.createIfcShapeRepresentation(f.by_type('IfcRepresentationContext')[0], 'Body', 'SurfaceModel', [shell])
         pdf = f.createIfcProductDefinitionShape(None, None, [rep])
-        prod = f.createIfcBuildingElementProxy(ifcopenshell.guid.new(), ObjectPlacement=f.createIfcLocalPlacement(RelativePlacement=f.createIfcAxis2Placement3D(f.createIfcCartesianPoint((0., 0., 0.)))), Representation=pdf)
+        prod = f.createIfcBuildingElementProxy(ifcopenshell.guid.new(), ObjectPlacement=f.createIfcLocalPlacement(RelativePlacement=f.createIfcAxis2Placement3D(f.createIfcCartesianPoint((0., 0., 0.)))), Name="Proxy", Representation=pdf)
 
         fail_or_pass = "fail" if not isv else "pass"
-        f.write(f'{fail_or_pass}-brp001-{shp_name}_{fn_name}.ifc')
+        model_file_name = f'{fail_or_pass}-brp001-{shp_name}_{fn_name}.ifc'
+        header = f.wrapped_data.header
+        header.file_name.name = model_file_name
+        f.write(model_file_name)
     for fni, shp in itertools.product((range(4)), (rect, zigzag, parallel_rect, parallel_concave, concave_parallel_crossing, concave_parallel_crossing, concave_parallel_almost_crossing, concave_non_parallel_crossing, single_point_touching, rect_redundant, rect_colinear_cross, rect_near_colinear_cross)):
         process(fni, shp)
+
 
 if __name__ == "__main__":
     main()

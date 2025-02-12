e7ae965858ce07084b72fa98a8d16e448f700051
Thomas Krijnen <t.krijnen@gmail.com>
2025-02-12 10:05:44 +0100

    Schema iden

--- features/steps/validation_handling.py
+++ features/steps/validation_handling.py
@@ -361,7 +361,7 @@ def expected_behave_output(context: Context, data: Any, is_observed : bool = Fal
         case str():
             if context.config.userdata.get("purepythonparser", False):
                 return {'schema_identifier': data}
-            elif data in [x.name() for x in ifcopenshell.ifcopenshell_wrapper.schema_by_name(context.model.schema).entities()]:
+            elif data in [x.name() for x in ifcopenshell.ifcopenshell_wrapper.schema_by_name(context.model.schema_identifier).entities()]:
                 return {'entity': data} # e.g. 'the type must be IfcCompositeCurve'
             else:
                 return {'value': data} # e.g. "The value must be 'Body'"

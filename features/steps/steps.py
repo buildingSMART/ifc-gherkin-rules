0c795a60069dc04ecb03cfd2db6eb5635e32f092
Geert Hesselink <geert.hess@gmail.com>
2025-02-24 23:42:29 +0100

    existance -> existence

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,3 +1,3 @@
 from givens import attributes, entities, relationships, values
-from thens import alignment, attributes, existance, geometry, nesting, reference, relations, values
+from thens import alignment, attributes, geometry, nesting, reference, relations, values, existence
 from steps import attribute_selection, entity_selection, model_traversal, representation

9cefddf80b5ba198e769c6717f39ed3737563ac8
Scott Lecher <civilx64@gmail.com>
2025-08-28 21:55:00 -0400

    revise ALB021 to properly handle 'approximate' alignment representations
    Fixes #428
    
    (IVS-583)

--- features/steps/steps/entity_selection.py
+++ features/steps/steps/entity_selection.py
@@ -4,21 +4,22 @@ from . import ValidationOutcome, OutcomeSeverity
 
 
 @gherkin_ifc.step("An .{entity_opt_stmt}.")
-@gherkin_ifc.step("An .{entity_opt_stmt}. ^{subtype_handling}^")
-def step_impl(context, entity_opt_stmt, subtype_handling=None):
+@gherkin_ifc.step("An .{entity_opt_stmt}. ^{subtype_handling}^ [{additional_prose_matching}]")
+def step_impl(context, entity_opt_stmt, subtype_handling=None, additional_prose_matching=None,):
     """
     Generic step, typically used as an initial or second Given statement to select entities.
     
     Examples:
         Given an .IfcAlignment.
-        Given an .IfcRoot. with subtypes
+        Given an .IfcRoot. ^with subtypes^
         Given an .entity instance.
+        Given an .IfcAlignment. [with business logic and geometric representation]
         
     The last example returns everything in the model.
     """
 
     if entity_opt_stmt == "entity instance":
-        instances = list(context.model)
+        original_instances = list(context.model)
     else:
         entity = entity_opt_stmt
 
@@ -30,13 +31,28 @@ def step_impl(context, entity_opt_stmt, subtype_handling=None):
             case _:
                 include_subtypes = True
 
-        instances = context.model.by_type(entity, include_subtypes) or []
+        original_instances = context.model.by_type(entity, include_subtypes) or []
+        filtered_instances = []
 
-    if instances:
+        if (entity == 'IfcAlignment') and (additional_prose_matching is not None):
+            from utils import ifc43x_alignment_validation as ifc43
+
+            for align_inst in original_instances:
+                align = ifc43.entities.Alignment().from_entity(align_inst)
+                if (align.horizontal is not None) and (align.has_representation):
+                    filtered_instances.append(align_inst)
+                    print(align)
+
+            applicable_instances = filtered_instances
+        else:
+            applicable_instances = original_instances
+
+
+    if applicable_instances:
         context.applicable = getattr(context, 'applicable', True)
     else:
         context.applicable = False
 
     # yield instances
-    for inst in instances:
+    for inst in applicable_instances:
         yield ValidationOutcome(instance_id=inst, severity=OutcomeSeverity.PASSED)

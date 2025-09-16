ed1bce2e2f3ed239855d5c26883f2f7c892d35bf
Scott Lecher <civilx64@gmail.com>
2025-09-16 14:29:40 -0400

    WIP update
    - incorporate api call for outcomes
    - set up to delete old requests before uploading files
    
    (IVS-372)

--- api_integration_script.py
+++ api_integration_script.py
@@ -9,21 +9,35 @@ Manual cleanup includes
 
 You will then need to revise the name of the functional part in the call to `check_rule_results` below.
 """
-
+import sys
+from enum import Enum
 import functools
 import os
 from pathlib import Path
+from typing import List, Dict
+
 
 from dotenv import load_dotenv
 import httpx
 
-URL_STEM = "https://dev.validate.buildingsmart.org/api"
+class SeverityEnum(Enum):
+    NA = 0
+    EXECUTED = 1
+    PASSED = 2
+    WARNING = 3
+    ERROR = 4
+
+environment = 'DEV'
+environment = 'LOCAL'
+
+load_dotenv()
+URL_STEM = os.environ.get(f"{environment}_API_ENDPOINT")
+TOKEN = os.environ.get(f"{environment}_API_TOKEN")
 
 @functools.lru_cache(maxsize=1)
 def build_headers():
-    load_dotenv()
     return {
-        "Authorization": f"Token {os.environ.get('DEV_API_TOKEN')}",
+        "Authorization": f"Token {TOKEN}",
         "Content-Type": "application/json"
     }
 
@@ -53,6 +67,18 @@ def get_all_requests():
 
     return resp
 
+def delete_request_by_id(request_id: str) -> None:
+    hdr = build_headers()
+    params = {"public_id": request_id}
+    resp = httpx.delete(
+        url=f"{URL_STEM}/validationrequest/{request_id}",
+        headers=hdr,
+    )
+    resp.raise_for_status()
+
+def delete_requests(requests: List[Dict]):
+    for request in requests:
+        delete_request_by_id(request_id=request["public_id"])
 
 def tasks_by_request_id(request_id: str):
     hdr = build_headers()
@@ -73,6 +99,20 @@ def tasks_by_request_id(request_id: str):
             return None
 
 
+def outcomes_by_request_id(request_id: str):
+    hdr = build_headers()
+    params = {"request_public_id": request_id}
+    resp = httpx.get(
+        url=f"{URL_STEM}/validationoutcome",
+        headers=hdr,
+        params=params,
+        timeout=60,
+    )
+
+    resp.raise_for_status()
+    return resp.json()
+
+
 def model_by_id(model_id: str):
     hdr = build_headers()
     resp = httpx.get(
@@ -83,42 +123,64 @@ def model_by_id(model_id: str):
     return resp.json()
 
 
-def check_rule_results(rule_code: str):
-    task_types_to_check = ["INDUSTRY", "NORMATIVE_IA", "NORMATIVE_IP"]
+def find_previous_validation_requests(rule_code: str) -> List[Dict]:
+    """
+    Finds all previous validation requests for the provided rule code and deletes them.
+    """
     jdata = get_all_requests().json()
-    rule_code_records = list()
+    rule_code_validation_requests = list()
     if len(jdata) == 0:
         print("[INFO] No validation results found!")
     else:
         for rec in jdata:
             filename = rec['file']
             if f"fail-{rule_code.lower()}" in filename or f"pass-{rule_code.lower()}" in filename:
-                rule_code_records.append(rec)
+                rule_code_validation_requests.append(rec)
 
-    num_rule_code_records = len(rule_code_records)
-    if num_rule_code_records == 0:
+    num_rule_code_requests = len(rule_code_validation_requests)
+    if num_rule_code_requests == 0:
         print(f"[INFO] No validation results found for {rule_code}!")
+        return list()
     else:
-        print(f"[INFO] Checking {len(rule_code_records)} results for {rule_code}...")
-        for rec in rule_code_records:
-            status = None
-            task_data = tasks_by_request_id(rec['public_id'])
-            for td in task_data:
-                task_type = td['type']
-                if task_type in task_types_to_check:
-                    if rule_code in td['status_reason']:
-                        if task_type == "INDUSTRY":
-                            status = "WARN"
-                        else:
-                            status = "FAIL"
+        print(f"[INFO] Found {num_rule_code_requests} validation requests for {rule_code}!")
+        return rule_code_validation_requests
 
-            if not status:
-                status = "PASS"
+
+
+
+def check_rule_results(validation_requets: List[Dict]):
+
+        print(f"[INFO] Checking {len(rule_code_validation_requests)} results for {rule_code}...")
+        for rec in rule_code_validation_requests:
+            status = None
+            print(f"[INFO] Getting outcomes for '{rec['file']}'...")
+            outcomes = outcomes_by_request_id(rec['public_id'])
+
+            severity = SeverityEnum(0)
+            for outcome in outcomes:
+                feature = outcome['feature']
+                if feature:
+                    feature_code = feature.split(" ")[0]
+                    if feature_code == rule_code:
+                        severity = SeverityEnum(outcome['severity'])
 
             filename = Path(rec['file']).stem
             expected_status = filename[:4].upper()
 
-            if status == expected_status:
+            match expected_status:
+                case "PASS":
+                    if severity in [SeverityEnum.EXECUTED, SeverityEnum.PASSED]:
+                        outcome_matches_expected = True
+                case "FAIL":
+                    if severity in [SeverityEnum.ERROR, SeverityEnum.WARNING]:
+                        outcome_matches_expected = True
+                case "NA":
+                    if severity == SeverityEnum.EXECUTED:
+                        outcome_matches_expected = True
+                case _:
+                    outcome_matches_expected = False
+
+            if outcome_matches_expected:
                 print(f"[INFO] '{rec['file']}' result matches expected status '{expected_status}'")
             else:
                 print("*** !!! ***")
@@ -127,6 +189,10 @@ def check_rule_results(rule_code: str):
 
 
 if __name__ == "__main__":
-    check_rule_results(rule_code="ALS005")
+    prev_requests = find_previous_validation_requests(rule_code="ALB021")
+    delete_requests(prev_requests)
+    print(prev_requests)
+
+    # check_rule_results(rule_code="ALB031")
     # get_all_outcomes()
     # get_all_requests()
